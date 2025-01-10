4c5f44466ac5fb1808febab821945a7ae2d29a57
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-10 23:25:09 +0530

    Cors updated

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 3000;
 const db = require("./db");
 app.use(express.json()); // Middleware to parse JSON request bodies
 const router = require("./Routes/todoRoutes");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL in production
+  })
+);
 // db;
 app.get("/", (req, res) => {
   res.status(200).json("Ok Home PAge");
