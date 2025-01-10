00f5ecdb5b1bb780ef0c16500e6adf1f3e21e8f4
Abusad Ahmad <ahmadabusad14@gmail.com>
2025-01-10 22:27:55 +0530

    Update multiple lines in backend and frontend for integration

--- Routes/todoRoutes.js
+++ Routes/todoRoutes.js
@@ -1,8 +1,13 @@
-const { createTodo, getTodo } = require("../controller/todoController");
+const {
+  createTodo,
+  getTodo,
+  getOneTodo,
+} = require("../controller/todoController");
 
 const router = require("express").Router();
 
 router.get("/", getTodo);
+router.get("/singleData/:id", getOneTodo);
 router.post("/", createTodo);
 
 module.exports = router;
--- controller/todoController.js
+++ controller/todoController.js
@@ -1,13 +1,6 @@
 const todoSchema = require("../model/todoSchema");
 
-exports.getTodo = async (req, res) => {
-  const data = await todoSchema.find();
-  res.status(201).json({
-    // message: "Todo created successfully",
-    data,
-  });
-};
-
+// !Api to create a todo task
 exports.createTodo = async (req, res) => {
   try {
     const newTodo = await todoSchema.create(req.body);
@@ -19,5 +12,24 @@ exports.createTodo = async (req, res) => {
     });
   } catch (error) {
     console.log("In try catch block", error);
+  }
+};
+
+// !Api to get All tasks
+exports.getTodo = async (req, res) => {
+  const data = await todoSchema.find();
+  res.status(201).json({
+    // message: "Todo created successfully",
+    data,
+  });
+};
+
+// !Api to get one task
+exports.getOneTodo = async (req, res) => {
+  try {
+    const oneData = await todoSchema.findById(req.params.id);
+    res.status(201).json(oneData);
+  } catch (error) {
+    console.log("Error in catch block", error);
   }
 };
